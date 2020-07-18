0a589d519d630e6bd7f636ddc635dc5bbe29ef93
brandonricharda <brandonricharda@gmail.com>
2020-07-17 22:27:16 -0400

    Created display method.

--- lib/board.rb
+++ lib/board.rb
@@ -1,34 +1,90 @@
 class Board 
 
-    attr_accessor :spaces
+    attr_accessor :positions, :player, :computer
 
     def initialize
-        cols = (0..6).to_a
-        @spaces = {}
+        @positions = {}
+        @player = "X"
+        @computer = "O"
+        (0..6).to_a.each do |row|
+            positions[row] = Array.new(6, nil)
+        end
+    end
 
-        cols.each do |item|
-            spaces[item] = Array.new(6, nil)
+    def place_piece(column, symbol)
+        return if !column || !(0..6).include?(column)
+        cell = nil
+        #identifies whether the column is full
+        full = @positions[column][0]
+        #finds the last non-nil row in the chosen column (array), then adds the player's piece on top of it
+        @positions[column].each_with_index do |row, index|
+            break if cell || full
+            if row
+                cell = (index - 1)
+            elsif index == @positions[column].length - 1
+                cell = index
+            end
+        end
+
+        if cell
+            @positions[column][cell] = symbol
+            #returns the index at which the new piece has been added
+            @positions[column].index(symbol)            
         end
 
     end
 
-    def place_piece(column)
-        spot = nil
-        #finds the last non-nil row in the chosen column, then adds the player's piece on top of it
-        @spaces[column].each_with_index do |row, index|
-            if row
-                spot = (index - 1)
-            elsif index == @spaces[column].length - 1
-                spot = index
+    def computer_move
+        col = nil
+        until place_piece(col, @computer)
+            col = rand(0..6)
+        end
+        col
+    end
+
+    def human_move
+        p "Choose a column (0-6)."
+        col = gets.chomp
+        until col.match?(/^[0-9]/) && place_piece(col.to_i, @player)
+            p "Please enter a number between 0 and 6 to represent a column that is not full."
+            col = gets.chomp
+        end
+        col.to_i
+    end
+
+    def full?
+        moves = []
+        @positions.each do |key, value|
+            value.each do |col|
+                moves << col
             end
         end
-        @spaces[column][spot] = "X"
-        #returns the index at which the new piece has been added
-        @spaces[column].index("X")
+        moves.all? { |value| value }
+    end
+
+    def display
+        interface = {}
+        (0..5).to_a.each do |row|
+            interface[row] = Array.new
+        end
+
+        @positions.each do |key, col|
+            col.each_with_index do |row, index|
+                interface[index] << row
+            end
+        end
+
+        interface.each do |key, row|
+            p row
+        end
+
     end
 
 end
 
 test = Board.new
-test.place_piece(0)
-test.place_piece(0)
+
+until 1 == 2
+    test.human_move
+    test.display
+end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,4 +1,5 @@
 require "./lib/board.rb"
+require "./lib/game.rb"
 
 describe Board do
 
@@ -15,9 +16,61 @@ describe Board do
     describe "#place_piece" do
         it "adds a piece to the last available row of a given column" do
             test = Board.new
-            expect(test.place_piece(1)).to eql(5)
-            expect(test.place_piece(1)).to eql(4)
+            expect(test.place_piece(1, test.player)).to eql(5)
+            expect(test.place_piece(1, test.player)).to eql(4)
+            expect(test.place_piece(1, test.player)).to eql(3)
+            expect(test.place_piece(1, test.player)).to eql(2)
+            expect(test.place_piece(1, test.player)).to eql(1)
+            expect(test.place_piece(1, test.player)).to eql(0)
         end
+
+        it "returns nil when a column is full" do
+            test = Board.new
+            expect(test.place_piece(1, test.player)).to eql(5)
+            expect(test.place_piece(1, test.player)).to eql(4)
+            expect(test.place_piece(1, test.player)).to eql(3)
+            expect(test.place_piece(1, test.player)).to eql(2)
+            expect(test.place_piece(1, test.player)).to eql(1)
+            expect(test.place_piece(1, test.player)).to eql(0)
+            expect(test.place_piece(1, test.player)).to eql(nil)
+        end
+    end
+
+    describe "#computer_move" do
+        it "makes a move for the computer" do
+            test = Board.new
+            expect(test.computer_move).to be_between(0, 6)
+        end
+    end
+
+    describe "#human_move" do
+        it "makes a move for the player" do
+            test = Board.new
+            expect(test.human_move).to be_between(0, 6)
+        end
+    end
+
+    describe "#full?" do
+        it "identifies when a board is full" do
+            test = Board.new
+            42.times { test.computer_move }
+            expect(test.full?).to eql(true)
+        end
+
+        it "identifies when a board is not full" do
+            test = Board.new
+            41.times { test.computer_move }
+            expect(test.full?).to eql(false)
+        end
+    end
+
+end
+
+describe Game do
+
+    it "assigns Board to the right variable" do
+        test = Game.new
+        expect(test.board.class).to eql(Board)
     end
 
 end
